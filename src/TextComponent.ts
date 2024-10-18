f68fd721f07db5ab5351aac3dc6e34d688224b62
Zefir Kirilov <contact@zefir.pro>
2024-10-18 14:34:16 +0300

    remove ignored parameters from `TextComponent#append`

--- src/TextComponent.ts
+++ src/TextComponent.ts
@@ -33,7 +33,7 @@ export class TextComponent extends NodeComponent<Text> {
 	 *
 	 * @throws {@link !DOMException} Always
 	 */
-	public override append(...components: NodeComponent<any>[]): typeof this {
+	public override append(): typeof this {
 		throw new DOMException(`TextComponent.append: Cannot add children to a ${this.constructor.name}`);
 	}
 
