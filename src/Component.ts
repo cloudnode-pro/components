c5481d273df240af16bbc6b298974a3bf620fac2
Kai <me@kai.enterprises>
2025-03-14 23:26:32 +0100

    add a dot after the end of the sentence

--- src/Component.ts
+++ src/Component.ts
@@ -100,7 +100,7 @@ export class Component<T extends HTMLElement = HTMLElement> extends ElementCompo
 
     /**
      * Puts the element into
-     * {@link https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/focus Focus}
+     * {@link https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/focus focus}.
      * @param options - An optional object for controlling aspects of the focusing process.
      */
     public focus(options?: FocusOptions): typeof this {
